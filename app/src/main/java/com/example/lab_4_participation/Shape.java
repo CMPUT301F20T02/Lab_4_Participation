5d6b317191b847619500508596eb191a80814fff
jackie0608 <yiyang9@ualberta.ca>
2020-10-04 21:37:43 -0600

    Added Color Field

--- app/src/main/java/com/example/lab_4_participation/Shape.java
+++ app/src/main/java/com/example/lab_4_participation/Shape.java
@@ -3,5 +3,5 @@ package com.example.lab_4_participation;
 public abstract class Shape {
     private int x;
     private int y;
-    private String color = "white";
+    private String color = "WHITE";
 }
